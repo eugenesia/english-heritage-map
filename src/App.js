b2d01efa47e8734135bc4bbc91d15fcc3b1d153e
Eugene Sia <eugene@eugenesia.co.uk>
2017-08-31 15:28:17 +0100

    Add Google Map in React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GettingStartedExample from './GettingStartedExample';
+import _ from 'lodash';
+
+const markers = [{
+	position: {
+		lat: 25.0112183,
+		lng: 121.52067570000001,
+	},
+	key: `Taiwan`,
+	defaultAnimation: 2,
+}];
 
 class App extends Component {
   render() {
@@ -13,6 +24,21 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+				<div style={{height: '400px', width: '400px'}}>
+					<GettingStartedExample
+						containerElement={
+							<div style={{ height: `100%` }} />
+						}
+						mapElement={
+							<div style={{ height: `100%` }} />
+						}
+						//onMapLoad={_.noop}
+						//onMapClick={_.noop}
+						markers={markers}
+						//onMarkerRightClick={_.noop}
+            toast={console.log}
+					/>
+				</div>
       </div>
     );
   }
---
+++ src/GettingStartedExample.js
@@ -0,0 +1,114 @@
+/* global google */
+import _ from "lodash";
+
+import { default as React, Component } from "react";
+
+import Helmet from 'react-helmet';
+
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+const googleMapURL='https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=AIzaSyAxDD6e26vr391KYJ3-rflvfnNTNlGLTJk';
+
+
+// Wrap all `react-google-maps` components with `withGoogleMap` HOC
+// and name it GettingStartedGoogleMap.
+const GettingStartedGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={3}
+    defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+    onClick={props.onMapClick}
+    //googleMapURL={googleMapURL}
+  >
+    {props.markers.map((marker, index) => (
+      <Marker
+        {...marker}
+        onRightClick={() => props.onMarkerRightClick(index)}
+      />
+    ))}
+  </GoogleMap>
+));
+
+export default class GettingStartedExample extends Component {
+  state = {
+    markers: [{
+      position: {
+        lat: 25.0112183,
+        lng: 121.52067570000001,
+      },
+      key: `Taiwan`,
+      defaultAnimation: 2,
+    }],
+  };
+
+  handleMapLoad = this.handleMapLoad.bind(this);
+  handleMapClick = this.handleMapClick.bind(this);
+  handleMarkerRightClick = this.handleMarkerRightClick.bind(this);
+
+  handleMapLoad(map) {
+    this._mapComponent = map;
+    if (map) {
+      console.log(map.getZoom());
+    }
+  }
+
+  /*
+   * This is called when you click on the map.
+   * Go and try click now.
+   */
+  handleMapClick(event) {
+    const nextMarkers = [
+      ...this.state.markers,
+      {
+        position: event.latLng,
+        defaultAnimation: 2,
+        key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
+      },
+    ];
+    this.setState({
+      markers: nextMarkers,
+    });
+
+    if (nextMarkers.length === 3) {
+      this.props.toast(
+        `Right click on the marker to remove it`,
+        `Also check the code!`
+      );
+    }
+  }
+
+  handleMarkerRightClick(targetMarker) {
+    /*
+     * All you modify is data, and the view is driven by data.
+     * This is so called data-driven-development. (And yes, it's now in
+     * web front end and even with google maps API.)
+     */
+    const nextMarkers = this.state.markers.filter(marker => marker !== targetMarker);
+    this.setState({
+      markers: nextMarkers,
+    });
+  }
+
+  render() {
+    return (
+      <div style={{height: `100%`}}>
+        <Helmet
+          title="Getting Started"
+        />
+        <GettingStartedGoogleMap
+          containerElement={
+            <div style={{ height: `100%` }} />
+          }
+          mapElement={
+            <div style={{ height: `100%` }} />
+          }
+          onMapLoad={this.handleMapLoad}
+          onMapClick={this.handleMapClick}
+          markers={this.state.markers}
+          onMarkerRightClick={this.handleMarkerRightClick}
+        />
+      </div>
+    );
+  }
+}
+
