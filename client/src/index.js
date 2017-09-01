5ca1db16df78767e672106acc9681e1115d3dcc6
Eugene Sia <eugene@eugenesia.co.uk>
2017-09-01 08:34:00 +0100

    Disable service workers as Chrome only allows on https

--- client/src/index.js
+++ client/src/index.js
@@ -5,4 +5,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Disable service workers Chrome only allows on secure origins (https).
+//registerServiceWorker();
